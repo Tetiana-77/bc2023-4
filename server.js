41a616a59ad05e57b9db2be01363e400d044e847
Tetiana-77 <144829121+Tetiana-77@users.noreply.github.com>
2023-10-24 13:17:54 +0300

    Add files via upload

---
+++ server.js
@@ -0,0 +1,42 @@
+const http = require('http'); // http server
+const fs = require('fs'); // fs - file system
+const xml = require('fast-xml-parser'); //
+
+const server = http.createServer((req, res) => { // Створюємо HTTP сервер і визначаємо обробник запитів.
+  if (req.method === 'GET' && req.url === '/') { // Перевіряємо, чи запит є GET-запитом і чи він адресований кореневому шляху ('/').
+    fs.readFile('data.xml', 'utf-8', (err, data) => { 
+        //parse
+        const maxRate = findMaxRate(data);
+        console.log(maxRate);
+         
+      const builder = new xml.XMLBuilder(); // Створюємо екземпляр класу XMLBuilder, який дозволяє створювати XML-документи.
+      const xmlStr = builder.build({"data" : {"max_rate" : maxRate}}); // json - обєкт, обєкт склад з полів, є поле data, у полі дата є 
+      // поле max_rate, у полі max_rate є значення maxRate
+      res.writeHead(200, { 'Content-Type': 'application/xml' }); // 200 - OK
+        res.end(xmlStr); 
+    });
+  } 
+});
+
+function findMaxRate(xmlData) { 
+
+  const parser = new xml.XMLParser();
+  const rates = parser.parse(xmlData)["exchange"]["currency"]
+
+  let maxRate = 0; 
+
+  for (const rate of rates) { 
+    const rateValue = rate["rate"] 
+
+    if (rateValue > maxRate) { 
+      maxRate = rateValue; 
+    }
+  }
+
+  return maxRate; 
+}
+
+const port = 8000; 
+server.listen(port, () => { // listen - запускає сервер на порті 8000
+  console.log(`Server is running on port ${port}`); 
+});
